4debed3c5059a3d560370343509d9855c9cb4df0
Jinhan Du <jinhan.du@qq.com>
2022-11-03 16:50:08 +0200

    Remove the bugged aos class. Optimize the css-library input position.

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="For Search Engine Optimization">
+    
     <meta property="og:title" content="For Share Title " />
     <meta property="og:description" content="Share Description " />
     <meta property="og:type" content="website" />
     <meta property="og:image" content="http://my.image.url" />
     <meta property="og:url" content="https://duit.ee" />
     
-    <link rel="stylesheet" href="./aos-animation/aos.css">
-    <link rel="stylesheet" href="./css-library/normalize.css" />
     <link rel="stylesheet" href="./scss/style.css" />
     <title>Jinhan Du Project</title>
     <link rel="icon" type="image/png" href="./img/burger.png">
